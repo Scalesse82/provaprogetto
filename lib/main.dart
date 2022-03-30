9eaf030a93e83d8178661a36c46c873f59412acd
giuseppescalesse <scalesse82@gmail.com>
2022-03-30 15:53:13 +0200

    servizi rest e jsondecod

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:provaprogetto/user.dart';
 import 'objectbox.g.dart';
 import 'secondPage.dart';
@@ -49,6 +51,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final controller = TextEditingController();
 
+  List<User> list_user=[User(email: "email1",password: "passe1"),
+    User(password: "pass2",email: "email2"),
+    User(password: "pass245",email: "email245")];
+
 
   @override
     void initState() {
@@ -62,6 +68,17 @@ class _MyHomePageState extends State<MyHomePage> {
         });
       }
     });
+  }
+  
+  
+  Future<String?> callApi() async {
+    
+    final resp= await get(Uri.parse("https://api.agify.io/?name=peppe"));
+    if(resp.statusCode==200)
+    return resp.body;
+    else
+      return null;
+
   }
 
   void _incrementCounter() {
@@ -100,96 +117,140 @@ class _MyHomePageState extends State<MyHomePage> {
           Center(
             child: CircularProgressIndicator(),
           )
-          : Center(
+          : FutureBuilder(
+           future: callApi(),
+          builder: (contecx,snapshot){
+
+              if(!snapshot.hasData){
+                return Center(child: CircularProgressIndicator(
+                    color:Colors.blue));
+              }else{
+                print(snapshot.data.toString());
+                Map<String,dynamic> data=jsonDecode(snapshot.data.toString());
+                User peppe=User.from_json(data);
+                return Center(child: Text(peppe.email!,
+                style:TextStyle(fontSize: 40)));
+              }
+
+          }),
+
+
+     /* Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Container(
-          // width: 100,
-          height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding:  EdgeInsets.all(16.0),
+          child: Container(
+            // width: 100,
 
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-          Form(
-            key: _formKey,
-                child: Padding(
-                  padding: EdgeInsets.all(18.0),
-                  child: TextFormField(
-                    validator: (value){
-                      if(value==null||value==""){
-                        return "";
-                      }
-                      return null;
 
-                    },
-                    decoration:  InputDecoration(
-                      errorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.red)
-                      ),
-                      enabledBorder:OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
-                          borderSide: BorderSide(color: Colors.green)
-                      ) ,
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
-                          borderSide: BorderSide(color: Colors.blue)
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
-                          borderSide: BorderSide(color: Colors.green)
-                      ),
-                      prefixIcon: Icon(Icons.add),
-                      //prefix: Icon(Icons.add)
+            child:
+            ListView.builder(
+                itemCount: list_user.length,
+                itemBuilder: (BuildContext context,i){
 
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.red,
+                      child: Text(list_user[i].email!+" "+list_user[i].password!),
+                    ),
+                  );
+
+                })
+
+
+            /*Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+            Form(
+              key: _formKey,
+                  child: Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: TextFormField(
+                      validator: (value){
+                        if(value==null||value==""){
+                          return "";
+                        }
+                        return null;
+
+                      },
+                      decoration:  InputDecoration(
+                        errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red)
+                        ),
+                        enabledBorder:OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100),
+                            borderSide: BorderSide(color: Colors.green)
+                        ) ,
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100),
+                            borderSide: BorderSide(color: Colors.blue)
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100),
+                            borderSide: BorderSide(color: Colors.green)
+                        ),
+                        prefixIcon: Icon(Icons.add),
+                        //prefix: Icon(Icons.add)
+
+
+                      ),
+                      controller: controller,
+                      onChanged: (dio) {
+                        testo = dio;
+                        setState(() {});
+                        _formKey.currentState!.validate();
+                        print("testo: $testo");
+                      },
 
                     ),
-                    controller: controller,
-                    onChanged: (dio) {
-                      testo = dio;
-                      setState(() {});
-                      _formKey.currentState!.validate();
-                      print("testo: $testo");
-                    },
-
                   ),
                 ),
-              ),
-               const Text(
-                'You have pushed the button this many:',
-                style:TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orangeAccent,
+                 const Text(
+                  'You have pushed the button this many:',
+                  style:TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orangeAccent,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              Text(
-                _counter.toString(),
-                style: Theme.of(context).textTheme.headline4,
-              ),
+                Text(
+                  _counter.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                ),
 
-              IconButton(
-                color: Colors.red,
-                onPressed:(){
-                  User peppe=User(email: "prova2@gmail.com", password: "Pass");
-                  var rng = Random();
-                  peppe.id= 4;//rng.nextInt(5);
-                  _store.box<User>().remove(7);
-                  _incrementCounter();
+                IconButton(
+                  color: Colors.red,
+                  onPressed:(){
+                    User peppe=User(email: "prova2@gmail.com", password: "Pass");
+                    var rng = Random();
+                    peppe.id= 4;//rng.nextInt(5);
+                    _store.box<User>().remove(7);
+                    _incrementCounter();
 
-                  } ,
-                icon: Icon(Icons.edit),
-              )
-            ],
+                    } ,
+                  icon: Icon(Icons.edit),
+                ),
+
+
+              ],
+            ),*/
           ),
         ),
-      ),
+      ),*/
       floatingActionButton: FloatingActionButton(
         backgroundColor: testo == null || testo =="" ? Colors.blue : Colors.green,
         onPressed: () {
+
+          list_user.add(User(email: "peppe",password: "iughfiudhfui"));
+          setState(() {
+
+          });
          /* if(_formKey.currentState!.validate())
                 Navigator.push(
                     context,
@@ -197,11 +258,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         builder: (context) => SecondPage(
                               title: testo!,
                             )));*/
-                User peppe=User(email: "prova@gmail.com", password: "Pass");
+                /*User peppe=User(email: "prova@gmail.com", password: "Pass");
                 var rng = Random();
                 peppe.id= 0;//rng.nextInt(10);
                 int id=_store.box<User>().put(peppe);
-                _incrementCounter();
+                _incrementCounter();*/
 
               },
         tooltip: 'Increment',
--- lib/user.dart
+++ lib/user.dart
@@ -8,6 +8,13 @@ late int id;
 String? email;
 String? password;
 
+
 User({required this.email, required this.password});
+
+//metodo che da una mappa ,i genera un object
+static User from_json(dynamic data){
+  return User(password: data["name"],email: data["name"]);
+}
+
 
 }
